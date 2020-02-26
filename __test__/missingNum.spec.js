17dbaa69f0a700133607c9938e9e37117035ccae
Ivan <ivandaixivwork@gmail.com>
2020-02-26 13:30:57 -0800

    Adds tests for missingNum.spec.js

--- __test__/missingNum.spec.js
+++ __test__/missingNum.spec.js
@@ -6,4 +6,14 @@ describe("missingNum", () => {
       expect(missingNum([1, 3, 6, 4, 1, 2])).toEqual(5);
     });
   });
+  describe("when given empty array", () => {
+    it("should return undefined", () => {
+      expect(missingNum([])).toEqual(undefined);
+    });
+  });
+  describe("when missing an input", () => {
+    it("should return undefined", () => {
+      expect(missingNum()).toEqual(undefined);
+    });
+  });
 });
